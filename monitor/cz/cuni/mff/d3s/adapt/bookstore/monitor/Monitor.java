e032340321cf760734a2a3f71327ffbaf3a2dbd8
Vojtech Horky <vojtech.horky@gmail.com>
2012-06-28 10:33:53 +0200

    Monitor tries to conform to SLA

--- monitor/cz/cuni/mff/d3s/adapt/bookstore/monitor/Monitor.java
+++ monitor/cz/cuni/mff/d3s/adapt/bookstore/monitor/Monitor.java
@@ -1,29 +1,38 @@
 package cz.cuni.mff.d3s.adapt.bookstore.monitor;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.Collection;
 
 import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Requires;
 import org.apache.felix.ipojo.annotations.Validate;
 
 import cz.cuni.mff.d3s.adapt.bookstore.agent.data.Measurement;
 import cz.cuni.mff.d3s.adapt.bookstore.agent.data.MeasurementStorage;
 import cz.cuni.mff.d3s.adapt.bookstore.agent.util.AccessAgent;
+import cz.cuni.mff.d3s.adapt.bookstore.services.Replicable;
 
 @Component
 public class Monitor implements Runnable {
 
 	private final String BOOK_STORE_CLASS = "cz.cuni.mff.d3s.adapt.bookstore.store.SimpleBookStore";
-	private final String BOOK_STORE_INSTRUMENTED_METHOD = "buy";
-
+	private final String BOOK_STORE_INSTRUMENTED_METHOD = "fulltextSearch";
+	private final String PROBE_NAME = "class:" + BOOK_STORE_CLASS.replace('.', '/') + "#" + BOOK_STORE_INSTRUMENTED_METHOD;
+	
+	private final double SLA_MAX_RESPONSE_TIME = 4000.;
+	
+	@Requires
+	private Replicable replicable;
+	
 	public Monitor() {
 
 	}
 
 	@Override
 	public void run() {
+		Measurement measurement = MeasurementStorage.getBackend();
+		
 		System.err.printf("MonitoringClient.run()\n");
-
+		
 		/*
 		 * Give it some time to make sure other components would be fully
 		 * initialized.
@@ -33,26 +42,53 @@ public class Monitor implements Runnable {
 		/* Instrument the selected method. */
 		AccessAgent.instrumentMethod(BOOK_STORE_CLASS,
 				BOOK_STORE_INSTRUMENTED_METHOD);
-
+		
 		/* Give it some time to run smoothly... */
 		sleep(2);
 		/* ...and instrument the same again. That should fail. */
 		AccessAgent.instrumentMethod(BOOK_STORE_CLASS,
 				BOOK_STORE_INSTRUMENTED_METHOD);
 		
-		
-		/* Give it some time to collect data. */
-		sleep(15);
-		
 		/*
 		 * And now, act upon the data.
 		 */
-		Measurement measurement = MeasurementStorage.getBackend();
-		try {
-			measurement.dump(new PrintWriter(System.out));
-		} catch (IOException e) {
-			e.printStackTrace();
+		while (true) {
+			long now = System.currentTimeMillis();
+			long interval = 1000 * 5;
+			Collection<Long> recentData = measurement.get(PROBE_NAME, now - interval, now);
+			
+			double recentMean = computeMeanUs(recentData);
+			
+			System.err.printf("          now=%d (recent=%1.0fus [%d samples])\n",
+					now,
+					recentMean, recentData.size());
+			
+			if (recentMean * 1.05 > SLA_MAX_RESPONSE_TIME) {
+				System.err.printf("WARNING: SLA probably violated (starting new instance)!\n");
+				replicable.startInstance();
+			}
+			
+			if (recentMean * 1.8 < SLA_MAX_RESPONSE_TIME) {
+				System.err.print("Probably too many resources, stopping some.\n");
+				replicable.stopInstance();
+			}
+			
+			sleep(5);
 		}
+		
+	}
+	
+	private double computeMeanUs(Collection<Long> input) {
+		int size = input.size();
+		if (size == 0) {
+			return Double.NaN;
+		}
+		
+		double sum = 0;
+		for (Long l : input) {
+			sum += l / 1000;
+		}
+		return sum / size;
 	}
 
 	private void sleep(int sec) {
