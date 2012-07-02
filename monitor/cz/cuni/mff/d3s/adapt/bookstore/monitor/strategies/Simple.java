4d39ca9c3c7fd90d2bdf7f30a52951e3a5312da7
Vojtech Horky <vojtech.horky@gmail.com>
2012-07-02 14:50:08 +0200

    Simple strategy can stop instances

--- monitor/cz/cuni/mff/d3s/adapt/bookstore/monitor/strategies/Simple.java
+++ monitor/cz/cuni/mff/d3s/adapt/bookstore/monitor/strategies/Simple.java
@@ -10,7 +10,9 @@ import cz.cuni.mff.d3s.spl.adapt.SlidingTimeSlotDataSource;
 public class Simple implements Strategy {
 	
 	private SlaFormula sla = null;
+	private SlaFormula stopInstanceContract = null;
 	private Replicable target = null;
+	private int instanceCount = 1;
 
 	@Override
 	public void init(Measurement measurement, String probeName,
@@ -21,6 +23,9 @@ public class Simple implements Strategy {
 		
 		sla = new SlaFormula(slaTimeMicros * 1000);
 		sla.bindDataSource(recentData);
+		
+		stopInstanceContract = new SlaFormula(slaTimeMicros * 1000 / 2);
+		stopInstanceContract.bindDataSource(recentData);
 	}
 	
 	@Override
@@ -29,6 +34,15 @@ public class Simple implements Strategy {
 		if (slaCompliance == ContractCompliance.VIOLATES) {
 			System.err.printf("SLA probably violated!\n");
 			target.startInstance();
+			instanceCount++;
+		}
+		
+		if (instanceCount > 1) {
+			ContractCompliance stopInstanceCompliance = stopInstanceContract.checkContract();
+			if (stopInstanceCompliance == ContractCompliance.COMPLIES) {
+				System.err.printf("Too many resources, freeing some.\n");
+				target.stopInstance();
+			}
 		}
 	}
 
