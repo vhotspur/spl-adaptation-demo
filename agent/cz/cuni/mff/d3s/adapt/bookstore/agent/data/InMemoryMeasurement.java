63c67001cf1e78e47e6984217294bb78d10b74c7
Vojtech Horky <vojtech.horky@gmail.com>
2012-07-02 14:42:46 +0200

    Remove unused import

--- agent/cz/cuni/mff/d3s/adapt/bookstore/agent/data/InMemoryMeasurement.java
+++ agent/cz/cuni/mff/d3s/adapt/bookstore/agent/data/InMemoryMeasurement.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
